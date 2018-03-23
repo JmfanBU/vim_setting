c6d6bf6d7748e7be1fdd8ac96f817a939fd92b33
Jiameng Fan <jmfan@bu.edu>
2018-03-22 23:11:23 -0400

    Modification

---
+++ setup.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+curl -L https://bit.ly/janus-bootstrap | bash
+
+ln -sf "$(pwd)"/vimrc.before ~/.vimrc.before
+ln -sf "$(pwd)"/vimrc.after ~/.vimrc.after
+ln -sf "$(pwd)"/janus ~/.janus
+
+git submodule update --init --recursive
